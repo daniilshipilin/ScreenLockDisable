37a4a351fbd14ebe65d9b10836ee0615a76c2771
Daniil Shipilin <daniil.shipilin@gmail.com>
2019-04-03 21:12:38 +0300

    Refacto

--- ScreenLockDisable/ScreenLocker.cs
+++ ScreenLockDisable/ScreenLocker.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Runtime.InteropServices;
 using Topshelf;
 
@@ -6,36 +5,25 @@ namespace ScreenLockDisable
 {
     public class ScreenLocker : ServiceControl
     {
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+        [DllImport("kernel32.dll")]
+        static extern uint SetThreadExecutionState(uint esFlags);
 
-        [Flags]
-        enum EXECUTION_STATE : uint
-        {
-            ES_SYSTEM_REQUIRED = 0x00000001,
-            ES_DISPLAY_REQUIRED = 0x00000002,
-            // Legacy flag, should not be used.
-            // ES_USER_PRESENT   = 0x00000004,
-            ES_AWAYMODE_REQUIRED = 0x00000040,
-            ES_CONTINUOUS = 0x80000000,
-        }
-
-        private static void ScreenLockDisable()
-        {
-            if (SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
-                                        | EXECUTION_STATE.ES_DISPLAY_REQUIRED
-                                        | EXECUTION_STATE.ES_SYSTEM_REQUIRED
-                                        | EXECUTION_STATE.ES_AWAYMODE_REQUIRED) == 0) //Away mode for Windows >= Vista
-            {
-                SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
-                                        | EXECUTION_STATE.ES_DISPLAY_REQUIRED
-                                        | EXECUTION_STATE.ES_SYSTEM_REQUIRED); //Windows < Vista, forget away mode
-            }
-        }
+        const uint ES_SYSTEM_REQUIRED   = 0x00000001;
+        const uint ES_DISPLAY_REQUIRED  = 0x00000002;
+        //const uint ES_USER_PRESENT    = 0x00000004, // legacy flag
+        const uint ES_AWAYMODE_REQUIRED = 0x00000040;
+        const uint ES_CONTINUOUS        = 0x80000000;
 
         public bool Start(HostControl hostControl)
         {
-            ScreenLockDisable();
+            // away mode for Windows >= Vista
+            uint res = SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED | ES_AWAYMODE_REQUIRED);
+
+            if (res == 0)
+            {
+                // Windows < Vista, forget away mode
+                SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED);
+            }
 
             return (true);
         }
