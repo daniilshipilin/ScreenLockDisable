566ec0eed63b86b646a2dadabda5a5e9b8299c02
Daniil Shipilin <daniil.shipilin@gmail.com>
2019-10-15 13:01:41 +0200

    Added program args launch info; Info text changed.

--- ScreenLockDisable/Program.cs
+++ ScreenLockDisable/Program.cs
@@ -11,11 +11,11 @@ namespace ScreenLockDisable
 
         const string VERSION_INFO = "";
 
-        #if DEBUG
+#if DEBUG
         const string BUILD_CONFIGURATION = " [Debug]";
-        #else
+#else
         const string BUILD_CONFIGURATION = "";
-        #endif
+#endif
 
         #endregion
 
@@ -23,13 +23,8 @@ namespace ScreenLockDisable
 
         private static Version Ver { get; } = new AssemblyName(Assembly.GetExecutingAssembly().FullName).Version;
         public static string ProgramVersion { get; } = $"{Ver.Major}.{Ver.Minor}.{Ver.Build}";
-        public static string ProgramBaseDirectory { get; } = AppDomain.CurrentDomain.BaseDirectory;
-        public static string ProgramPath { get; } = Assembly.GetEntryAssembly().Location;
-        //public static string ProgramSHA256 { get; } = Utils.CalculateFileSHA256(ProgramPath);
         public static string ProgramName { get; } = Assembly.GetExecutingAssembly().GetName().Name;
         public static string ProgramHeader { get; } = $"{ProgramName} v{ProgramVersion}{VERSION_INFO}{BUILD_CONFIGURATION}";
-        //public static string ProgramBuild { get; } = $"Build: {Properties.Resources.BuildTime} (UTC) CLR: {Assembly.GetExecutingAssembly().ImageRuntimeVersion} SHA256: {ProgramSHA256}";
-        //public static string ProgramLastCommit { get; } = $"Commit: {Properties.Resources.LastCommit}";
         public static string ProgramAuthor { get; } = "Author: Daniil Shipilin";
 
         #endregion
@@ -46,20 +41,27 @@ namespace ScreenLockDisable
         enum EXECUTION_STATE : uint
         {
             ES_AWAYMODE_REQUIRED = 0x00000040, // This flag must be combined with ES_CONTINUOUS. If the machine is configured to allow it, this indicates that the thread requires away mode. When in away mode the computer will appear to sleep as normal. However, the thread will continue to execute even though the computer has partially suspended. As this flag gives the false impression that the computer is in a low power state, you should only use it when absolutely necessary.
-            ES_CONTINUOUS        = 0x80000000, // This flag is used to specify that the behaviour of the two previous flags is continuous. Rather than resetting the idle timers once, they are disabled until you specify otherwise. Using this flag means that you do not need to call SetThreadExecutionState repeatedly.
-            ES_DISPLAY_REQUIRED  = 0x00000002, // This flag indicates that the display is in use. When passed by itself, the display idle timer is reset to zero once. The timer restarts and the screensaver will be displayed when it next expires.
-            ES_SYSTEM_REQUIRED   = 0x00000001  // This flag indicates that the system is active. When passed alone, the system idle timer is reset to zero once. The timer restarts and the machine will sleep when it expires.
+            ES_CONTINUOUS = 0x80000000, // This flag is used to specify that the behaviour of the two previous flags is continuous. Rather than resetting the idle timers once, they are disabled until you specify otherwise. Using this flag means that you do not need to call SetThreadExecutionState repeatedly.
+            ES_DISPLAY_REQUIRED = 0x00000002, // This flag indicates that the display is in use. When passed by itself, the display idle timer is reset to zero once. The timer restarts and the screensaver will be displayed when it next expires.
+            ES_SYSTEM_REQUIRED = 0x00000001  // This flag indicates that the system is active. When passed alone, the system idle timer is reset to zero once. The timer restarts and the machine will sleep when it expires.
         }
 
         static void Main(string[] args)
         {
+            if (args.Length == 1 && args[0].Equals("/?"))
+            {
+                Console.WriteLine(ProgramHeader);
+                Console.WriteLine(ProgramAuthor);
+                return;
+            }
+
             // launch console window minimized
             ShowWindow(Process.GetCurrentProcess().MainWindowHandle, SW_SHOWMINIMIZED);
             Console.Title = ProgramHeader;
             Console.CursorVisible = false;
             Console.WriteLine("'Q': key to exit");
-            Console.WriteLine("'D': key to disable screen lock (default)");
-            Console.WriteLine("'R': key to re-enable (reset) screen lock");
+            Console.WriteLine("'D': key to disable screen lock");
+            Console.WriteLine("'E': key to enable screen lock");
 
             ScreenLockDisable();
 
@@ -68,7 +70,7 @@ namespace ScreenLockDisable
                 var key = Console.ReadKey(true).Key;
 
                 if (key == ConsoleKey.Q) { break; }
-                else if (key == ConsoleKey.R) { ScreenLockReset(); }
+                else if (key == ConsoleKey.E) { ScreenLockEnable(); }
                 else if (key == ConsoleKey.D) { ScreenLockDisable(); }
             }
         }
@@ -80,11 +82,11 @@ namespace ScreenLockDisable
             Console.WriteLine("Screen lock disabled");
         }
 
-        private static void ScreenLockReset()
+        private static void ScreenLockEnable()
         {
             // Re-enabling the screensaver requires that we clear the ES_DISPLAY_REQUIRED state flag. We can do this by passing the ES_CONTINUOUS flag alone
             SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
-            Console.WriteLine("Screen lock reset");
+            Console.WriteLine("Screen lock enabled");
         }
     }
 }
